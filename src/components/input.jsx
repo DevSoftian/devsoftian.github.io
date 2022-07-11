946c1defe68a6358b8b104352398342e3e19156d
JG Stephens <DevSoftGuy@protonmail.com>
2022-07-11 00:12:20 -0500

    Changed class to classname and addeed an email field to loginForm.

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -4,19 +4,19 @@ const Input = ({ name, label, value, onChange, formType }) => {
   const helpLabel = `${name}Help`;
 
   return (
-    <div class="form-floating mb-3 mx-auto">
+    <div className="form-floating mb-3 mx-auto">
       <label htmlFor={name}>{label}</label>
       <input
         value={value}
         name={name}
         onChange={onChange}
         type={formType}
-        class="form-control"
+        className="form-control"
         id={name}
         aria-describedby={helpLabel}
         placeholder={label}
       />
-      <small id={helpLabel} class="form-text text-muted">
+      <small id={helpLabel} className="form-text text-muted">
         Please enter your {name}
       </small>
     </div>
--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -2,18 +2,19 @@ import React, { Component } from "react";
 import Navbar from "./navbar";
 import { bootstrap } from "bootstrap";
 import Input from "./input";
+import * as userService from "../services/userService";
 
 // username = React.createRef();
 // password = React.createRef();
 
 class LoginForm extends Component {
   state = {
-    account: { username: "", password: "" },
+    account: { username: "", password: "", name: "" },
   };
 
   //Handles form submission.
-  handleSubmit = (e) => {
-    e.preventDefault(); //Prevents default behavior (submitting and reloading the whole page).
+  handleSubmit = async (e) => {
+    await userService.registerUser(this.state.account); // e.preventDefault(); //Prevents default behavior (submitting and reloading the whole page).
 
     console.log("Submitted");
   };
@@ -29,29 +30,36 @@ class LoginForm extends Component {
     const { account } = this.state;
 
     return (
-      <div class="text-center">
+      <div className="text-center">
         <Navbar />
         {/* Login box */}
-        <div class="container">
-          <div class="row">
-            <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
-              <div class="card border-0 shadow rounded-3 my-5">
-                <div class="card-body p-4 p-sm-5">
-                  <h5 class="card-title text-center mb-5 fw-light fs-5">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
+              <div className="card border-0 shadow rounded-3 my-5">
+                <div className="card-body p-4 p-sm-5">
+                  <h5 className="card-title text-center mb-5 fw-light fs-5">
                     Sign In
                   </h5>
                   <form onSubmit={this.handleSubmit}>
-                    <div class="form-floating mb-3">
+                    <div className="form-floating mb-3">
                       {/* Username input */}
                       <Input
                         name="username"
-                        label="Username"
+                        label="Email"
                         formType="text"
                         value={account.username}
                         onChange={this.handleChange}
                       />
                     </div>
                     {/* Password input */}
+                    <Input
+                      name="name"
+                      label="Name"
+                      formType="text"
+                      value={account.name}
+                      onChange={this.handleChange}
+                    />
                     <Input
                       name="password"
                       label="Password"
@@ -60,30 +68,30 @@ class LoginForm extends Component {
                       onChange={this.handleChange}
                     />
                     {/* Check Box */}
-                    <div class="form-check mb-3">
+                    <div className="form-check mb-3">
                       <input
-                        class="form-check-input"
+                        className="form-check-input"
                         type="checkbox"
                         value=""
                         id="rememberPasswordCheck"
                       />
                       <label
-                        class="form-check-label"
-                        for="rememberPasswordCheck"
+                        className="form-check-label"
+                        htmlFor="rememberPasswordCheck"
                       >
                         Remember password
                       </label>
                     </div>
                     {/* Submit button */}
-                    <div class="d-grid">
+                    <div className="d-grid">
                       <button
-                        class="btn btn-primary btn-login text-uppercase fw-bold"
+                        className="btn btn-primary btn-login text-uppercase fw-bold"
                         type="submit"
                       >
                         Submit
                       </button>
                     </div>
-                    <div class="p-3"></div>
+                    <div className="p-3"></div>
                   </form>
                 </div>
               </div>
